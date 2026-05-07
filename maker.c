80ce42c3a0fa1b8a1ed2182b1daac6a4e247d82d
acharya-dilip <acharyamission6@gmail.com>
2026-05-07 09:38:50 +0545

    init of a parentgrid for the maker window

--- maker.c
+++ maker.c
@@ -25,6 +25,10 @@ void openMaker() {
     gtk_window_set_default_size(GTK_WINDOW(maker.window),800,800);
     gtk_window_present(GTK_WINDOW(maker.window));
 
+    //Init for a grid
+    GtkWidget *gridParent = gtk_grid_new();
+    gtk_window_set_child(GTK_WINDOW(maker.window),gridParent);
+
     //Text view for entering the question
     maker.textviewQuestion = gtk_text_view_new();
     gtk_text_view_set_wrap_mode(GTK_TEXT_VIEW(maker.textviewQuestion),TRUE);
