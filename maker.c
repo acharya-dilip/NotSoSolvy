455550f651c252176d8333d09b9cfc602bc0e75e
acharya-dilip <acharyamission6@gmail.com>
2026-05-07 09:45:18 +0545

    attached the text view with the right size and specs and addded margins to the greid

--- maker.c
+++ maker.c
@@ -28,9 +28,20 @@ void openMaker() {
     //Init for a grid
     GtkWidget *gridParent = gtk_grid_new();
     gtk_window_set_child(GTK_WINDOW(maker.window),gridParent);
+    //Margins for the parent grid
+    gtk_widget_set_margin_top(gridParent,10);
+    gtk_widget_set_margin_bottom(gridParent,10);
+    gtk_widget_set_margin_start(gridParent,10);
+    gtk_widget_set_margin_end(gridParent,10);
 
     //Text view for entering the question
     maker.textviewQuestion = gtk_text_view_new();
     gtk_text_view_set_wrap_mode(GTK_TEXT_VIEW(maker.textviewQuestion),TRUE);
+    gtk_grid_attach(GTK_GRID(gridParent),maker.textviewQuestion,0,0,12,12);
+    gtk_widget_set_size_request(maker.textviewQuestion,780,260);
+
+
+
+
 
 }
