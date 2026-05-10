f3be9be10ae4fddf48c1e3fca01e191d030e4db5
acharya-dilip <acharyamission6@gmail.com>
2026-05-10 16:26:14 +0545

    removed the default size for the solver window

--- main.c
+++ main.c
@@ -103,7 +103,6 @@ static void activate (GtkApplication *app,gpointer user_data) {
     // The window for the solver
     solver.window = gtk_application_window_new(app);
     gtk_window_set_title(GTK_WINDOW(solver.window),"NotSoSolvy");
-        gtk_window_set_default_size(GTK_WINDOW(solver.window),800,800);
     gtk_window_present(GTK_WINDOW(solver.window));
 
     //Header bar for the buttons and stuff
